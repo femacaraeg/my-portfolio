4c7e2ef18a22157cc58d393a7380782e61d800da
Fe Macaraeg <femacaraeg@gmail.com>
2022-11-13 23:46:27 +0800

    Update dialog component.

--- src/components/ProjectDialog/ProjectDialog.js
+++ src/components/ProjectDialog/ProjectDialog.js
@@ -3,11 +3,50 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  // IconButton,
+  IconButton,
   Typography,
   Button,
 } from '@material-ui/core'
-// import CloseIcon from '@material-ui/icons/Close'
+import { withStyles } from '@material-ui/core/styles'
+import CloseIcon from '@material-ui/icons/Close'
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  title: {
+    fontWeight: '700',
+  },
+})
+
+const BootstrapDialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props
+  return (
+    <DialogTitle disableTypography id='dialog-title' className={classes.root}>
+      {children}
+      <IconButton
+        aria-label='close'
+        className={classes.closeButton}
+        onClick={onClose}
+      >
+        <CloseIcon />
+      </IconButton>
+    </DialogTitle>
+  )
+})
+
+const BootstrapDialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(DialogContent)
 
 const ProjectDialog = (props) => {
   const { isOpen, handleClose, project } = props
@@ -21,10 +60,12 @@ const ProjectDialog = (props) => {
         onClose={handleClose}
         aria-labelledby='dialog-with-title'
       >
-        <DialogTitle id='dialog-title'>
-          <Typography>{project.name}</Typography>
-        </DialogTitle>
-        <DialogContent>Description</DialogContent>
+        <BootstrapDialogTitle onClose={handleClose}>
+          <h3>{project.name}</h3>
+        </BootstrapDialogTitle>
+        <DialogContent>
+          <Typography>{project.description}</Typography>
+        </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
         </DialogActions>
